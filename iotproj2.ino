398f5a523c9f5eb1a329643a3411cc18146d22af
Ashwin Santhosh Nair <84934397+Ashwin-S-N@users.noreply.github.com>
2021-11-28 18:33:43 +0530

    Add files via upload

---
+++ iotproj2.ino
@@ -0,0 +1,50 @@
+const int TRIGGER = 2;  //D4
+const int ECHO = 0;  //D3
+int value;
+
+#define BLYNK_PRINT Serial    
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "7Zk_39HwJb-zuDZyDHTYxmjH3FjOe_Ld";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "SSAA";
+char pass[] = "kunjuammini1234";
+void setup() {
+  
+  Serial.begin (9600);
+  Blynk.begin(auth, ssid, pass);
+  pinMode(TRIGGER, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(BUILTIN_LED, OUTPUT);
+}
+
+void loop() {
+  
+  long duration, distance;
+  digitalWrite(TRIGGER, LOW);  
+  delayMicroseconds(2); 
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(TRIGGER, LOW);
+  duration = pulseIn(ECHO, HIGH);
+  distance= duration*0.034/2;
+  Serial.print(distance);
+  Serial.println("Centimeter:");
+  value=map(distance,0,11,0,100);
+  Blynk.virtualWrite(V5,distance);
+  delay(200);
+  if(distance<5){
+    Blynk.virtualWrite(V3,255);
+    Blynk.virtualWrite(V1,0);
+  }
+  else{
+    Blynk.virtualWrite(V1,255);
+    Blynk.virtualWrite(V3,0);
+  }
+  Blynk.run();
+}
